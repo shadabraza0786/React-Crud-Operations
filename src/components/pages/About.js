ca76fdfefacba2b5c074e061a76089891715d0d6
Shadab Raza <shadabraza500@gmail.com>
2022-10-22 15:07:52 +0530

    Chagnes in code Format

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,6 +5,10 @@ const About =()=>{
         <div className="container">
             <div className="py-4">
                 <h1>About page</h1>
+                <p className="lead mt-5">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
+                </p>
+
                 <p className="lead">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
                 </p>
