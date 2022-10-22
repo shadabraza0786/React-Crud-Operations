6045eb92739f5e0c368ff0ae75df196469185bfe
Shadab Raza <shadabraza500@gmail.com>
2022-10-22 15:21:19 +0530

    Changes in code Format

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -20,6 +20,10 @@ const About =()=>{
                 <p className="lead">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
                 </p>
+
+                <p className="lead">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
+                </p>
             </div>
         </div>
     );
