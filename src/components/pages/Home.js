48943d6ae87e456bf1981d0290fcc3663c8b35b0
Shadab Raza <shadabraza500@gmail.com>
2022-10-27 17:41:02 +0530

    Api integration with users

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,12 +6,12 @@ const Home = () => {
   const [users, setUser] = useState([]);
 
   const getUser = async () => {
-    const result = await axios.get("http://localhost:5000/users");
+    const result = await axios.get("https://curd-operations-api.herokuapp.com/users");
     setUser(result.data);
   };
 
   const deleteUser = async id => {
-    await axios.delete(`http://localhost:5000/users/${id}`);
+    await axios.delete(`https://curd-operations-api.herokuapp.com/users/${id}`);
     getUser();
 
   }
